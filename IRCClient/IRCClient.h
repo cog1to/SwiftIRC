76d513b813e9b3e1fb1fe6764ba9d5a0ab24a1ef
Alexander Rogachev <alexander.rogachev@akvelon.com>
2018-05-09 22:57:30 +0700

    Initial Commit

---
+++ IRCClient/IRCClient.h
@@ -0,0 +1,19 @@
+//
+//  IRCClient.h
+//  IRCClient
+//
+//  Created by Alexander Rogachev on 5/9/18.
+//  Copyright © 2018 Alexander Rogachev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IRCClient.
+FOUNDATION_EXPORT double IRCClientVersionNumber;
+
+//! Project version string for IRCClient.
+FOUNDATION_EXPORT const unsigned char IRCClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IRCClient/PublicHeader.h>
+
+
